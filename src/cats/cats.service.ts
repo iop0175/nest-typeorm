6fffca79652a36ab008783a3d231623ff55460bc
daegiEun <59227797+iop0175@users.noreply.github.com>
2025-08-21 15:47:09 +0900

    cc

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cat } from './entity/cats.entity';
+import { getConnection, Repository } from 'typeorm';
+import { promises } from 'dns';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class CatsService {
+    constructor(
+        @InjectRepository(Cat)
+        private catsRepository:Repository<Cat>,
+        private dataSource: DataSource,
+    ){}
+    findAll(): Promise<Cat[]>{
+        return this.catsRepository.find();
+    }
+    findOne(id:number): Promise<Cat|null>{
+        return this.catsRepository.findOne({where:{id}});
+    }
+    async create(cat:Cat):Promise<void>{
+        await this.catsRepository.save(cat);
+    }
+    async remove(id:number): Promise<void>{
+        await this.catsRepository.delete(id);
+    }
+    async update(id:number,cat:Cat):Promise<void>{
+        const existedCat = await this.findOne(id);
+        if(existedCat){
+            await this.dataSource
+            .createQueryBuilder()
+            .update(Cat)
+            .set({
+                name:cat.name,
+                age:cat.age,
+                breed:cat.breed
+            })
+            .where("id=:id",{id})
+            .execute()
+        }
+    }
+}
---
+++ src/cats/entity/cats.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Cat{
+    @PrimaryColumn()
+    id: number
+    @Column()
+    name:string
+    @Column()
+    age:number
+    @Column()
+    breed:string
+}
